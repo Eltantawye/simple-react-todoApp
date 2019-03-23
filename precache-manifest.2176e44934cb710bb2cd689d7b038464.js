40ddc979ccc6827c7e3042aac912f16a85ec9e16
mohamed eltantawy <m.eltantawy17@gmail.com>
2019-03-23 03:05:12 +0200

    Updates

---
+++ precache-manifest.2176e44934cb710bb2cd689d7b038464.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1659391d3cf49a1853d1",
+    "url": "/simple-react-todoapp/static/js/runtime~main.ad38e797.js"
+  },
+  {
+    "revision": "70f5a239ebab973729bb",
+    "url": "/simple-react-todoapp/static/js/main.fd9b72c0.chunk.js"
+  },
+  {
+    "revision": "e70a3cab4e4408c60fa4",
+    "url": "/simple-react-todoapp/static/js/2.10117d87.chunk.js"
+  },
+  {
+    "revision": "70f5a239ebab973729bb",
+    "url": "/simple-react-todoapp/static/css/main.cdbc26c7.chunk.css"
+  },
+  {
+    "revision": "74c0f04ac40d1b37819e56c10852401d",
+    "url": "/simple-react-todoapp/index.html"
+  }
+];
